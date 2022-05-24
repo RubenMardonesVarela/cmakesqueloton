f9171d46ab86d834ce43197024d2c4282887eca5
RubenMardonesVarela <ruben.mardones.varela@gmail.com>
2022-05-24 08:30:33 -0400

    Fix for windows build

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -4,7 +4,6 @@
 #include "spdlog/sinks/stdout_sinks.h"
 #include "spdlog/spdlog.h"
 #include <iostream>
-#include <unistd.h>
 
 int main(int argc, char *argv[]) {
   std::cout << "Welcome to basic cmake project with gtest and spdlog"
